596022faa793ba3c7a7e58ec77a8f8ce3cf9747b
Jonathan Hendler <jonathan.hendler@gmail.com>
2013-09-06 23:09:11 -0400

    simple socket connection to send string established

--- src/github.com/hendler/goling/client/client.go
+++ src/github.com/hendler/goling/client/client.go
@@ -32,22 +32,24 @@ func FreelingTest(){
     }
 
     fmt.Printf("Connected\n\n")
-    //fmt.Fprintf(conn, "RESET_STATS\n\n")
 
     //http://stackoverflow.com/questions/12359777/how-can-i-convert-a-null-terminated-string-in-a-byte-buffer-to-a-string-in-go
-
     //var zb = []byte{0}
 
-    fmt.Fprintf(conn, "RESET_STATS%c", '\x00')
+    //fmt.Fprintf(conn, "RESET_STATS%c", '\x00')
+    fmt.Fprintf(conn, "Every Good Boy Deserves Fudge.%c", '\x00')
    
-    //fmt.Fprintf(conn, zb)
     status, err := bufio.NewReader(conn).ReadString('\x00')
     if err != nil {
         // handle error
         fmt.Printf("ERROR READING\n\n")
     }
 
-    fmt.Printf("Results: %s\n\n", status)
+    fmt.Printf("\nResults: %s\n", status)
+
+
+    
+ 
      
 }
 
