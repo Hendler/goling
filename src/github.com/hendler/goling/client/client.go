dba58ae268a78996b6079bddef5482265c8cb1d3
Jonathan Hendler <jonathan.hendler@gmail.com>
2013-09-06 23:32:15 -0400

    resuable socket connection...

--- src/github.com/hendler/goling/client/client.go
+++ src/github.com/hendler/goling/client/client.go
@@ -15,42 +15,42 @@ import (
     "fmt"
     "net"
     "bufio"
+    _"io"
     _"os"
     "log"
 ) 
 
-func Hello() {
-    fmt.Printf("Hello, world.\n")
-}
 
-func FreelingTest(){
+func Init()(net.Conn){
     conn, err := net.Dial("tcp", "localhost:50005")
     if err != nil {
         // handle error
-        fmt.Printf("ERROR CONNECTING\n\n")
+        //fmt.Printf("ERROR CONNECTING\n\n")
         log.Fatal(err)
     }
+    return conn
+}
 
-    fmt.Printf("Connected\n\n")
-
+func Send( conn net.Conn , message string )(string){
+    //null terminated string
+    // //var zb = []byte{0}
     //http://stackoverflow.com/questions/12359777/how-can-i-convert-a-null-terminated-string-in-a-byte-buffer-to-a-string-in-go
-    //var zb = []byte{0}
-
-    //fmt.Fprintf(conn, "RESET_STATS%c", '\x00')
-    fmt.Fprintf(conn, "Every Good Boy Deserves Fudge.%c", '\x00')
-   
+    fmt.Fprintf(conn, "%s%c", message,  '\x00')
     status, err := bufio.NewReader(conn).ReadString('\x00')
     if err != nil {
         // handle error
-        fmt.Printf("ERROR READING\n\n")
+        log.Fatal(err)
     }
+    return status
+}
 
+func FreelingTest(){
+    conn := Init()
+    fmt.Printf("Connected\n\n")
+    status := Send( conn , "RESET_STATS")
     fmt.Printf("\nResults: %s\n", status)
-
-
-    
- 
-     
+    status = Send( conn , "Every Good Boy Deserves Fudge.")
+    fmt.Printf("\nResults: %s\n", status)
 }
 
 
